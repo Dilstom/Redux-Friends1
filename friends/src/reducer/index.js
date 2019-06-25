eaeac0f0904f67a1c93fd6bc30ac3e43cb49f207
Dil Sombayeva <dilyasom@gmail.com>
2019-06-25 15:40:36 -0700

    add reducers

--- friends/src/reducer/index.js
+++ friends/src/reducer/index.js
@@ -1,14 +1,29 @@
+import {
+ FETCH_FRIENDS,
+ ERROR,
+ SUCCESS,
+ LOGIN_SUCCESS,
+ LOGIN_START,
+} from '../actions';
+
 let defaultState = {
- friends: [
-  {
-   id: 1,
-   name: 'Jow',
-  },
- ],
+ friends: [],
+ loading: false,
+ error: '',
+ loggingIn: false,
 };
 
 export default function reducer(state = defaultState, action) {
  switch (action.type) {
+  case FETCH_FRIENDS:
+   return { ...state, friends: [], loading: true, error: '' };
+  case SUCCESS:
+   return { ...state, friends: action.payload, loading: false, error: '' };
+  case ERROR:
+   return { ...state, friends: [], loading: false, error: action.payload };
+  case LOGIN_START:
+   return { ...state, error: '', loggingIn: true };
+
   default:
    return state;
  }
