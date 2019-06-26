a050507c65211295c22a361e7297a797f7611efe
Dil Sombayeva <dilyasom@gmail.com>
2019-06-26 13:16:56 -0700

    pass creds to login action creator

--- friends/src/components/LoginForm.js
+++ friends/src/components/LoginForm.js
@@ -1,38 +1,70 @@
 import React, { Component } from 'react';
+import Loader from 'react-loader-spinner';
+import { connect } from 'react-redux';
+import { login } from '../actions';
 
 class LoginForm extends Component {
  state = {
-     credentials = {
-         username: '',
-         password: ''
-     }
+  credentials: {
+   username: '',
+   password: '',
+  },
  };
 
-handleChange = e => {
-    this.setState({
-        credentials: {
-            ...this.state.credentials,
-            [e.target.name]: e.target.value
-        }
-    })
-}
+ handleChange = e => {
+  this.setState({
+   credentials: {
+    ...this.state.credentials,
+    [e.target.name]: e.target.value,
+   },
+  });
+ };
 
  login = e => {
-     e.preventDefault();
-    // this.props.login(this.state.credentials)
- }
+  e.preventDefault();
+  // since we return axios.post call - we got a promise
+  this.props.login(this.state.credentials).then(res => {
+   if (res) {
+    this.props.history.push('/api/friends');
+   }
+  });
+ };
  render() {
+  console.log('this.props: ', this.props);
   return (
    <div>
-       <form onSubmit={this.login}>
-    <input name='username' value = {this.state.credentials.username} placeholder="username" onChange = {this.handleChange} />
-    <input name='password' value = {this.state.credentials.password} placeholder="password" onChange = {this.handleChange} />
-    <input placeholder="repeat password" />
-    <button>Submit</button>
-       </form>
+    <form onSubmit={this.login}>
+     <input
+      name="username"
+      value={this.state.credentials.username}
+      placeholder="username"
+      onChange={this.handleChange}
+     />
+     <input
+      name="password"
+      value={this.state.credentials.password}
+      placeholder="password"
+      onChange={this.handleChange}
+     />
+     <button>
+      {this.props.loggingIn ? (
+       <Loader type="ThreeDots" color="green" height="12" width="26" />
+      ) : (
+       'Log in'
+      )}
+     </button>
+    </form>
    </div>
   );
  }
 }
 
-export default LoginForm;
+const mapStateToProps = state => ({
+ error: state.error,
+ loggingIn: state.loggingIn,
+});
+
+export default connect(
+ mapStateToProps,
+ { login }
+)(LoginForm);
