be53e6a00be7f9f3a09221d9d532f4f097936d64
Dil Sombayeva <dilyasom@gmail.com>
2019-06-26 13:14:35 -0700

    implement login action creator

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,31 +1,52 @@
 import axios from 'axios';
-export const FETCH_FRIENDS = 'FETCH_FRIENDS';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+export const FETCH_START = 'FETCH_START';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_ERROR = 'FETCH_ERROR';
 export const ERROR = 'ERROR';
 export const SUCCESS = 'SUCCESS';
 export const LOADING = 'LOADING';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_START = 'LOGIN_START';
 
-export function getFriends() {
- return function(dispatch) {
-  dispatch({ type: FETCH_FRIENDS });
-  axios
-   .get('http://localhost:5000/api/friends')
-   .then(res => dispatch({ type: SUCCESS, payload: res.data }))
-   .catch(err => dispatch({ type: ERROR, payload: err }));
- };
-}
-
-// export function login() {
-//  return {
-//   type: LOGIN_SUCCESS,
-//   payload: {
-//    token:
-//     'esfeyJ1c2VySWQiOiJiMDhmODZhZi0zNWRhLTQ4ZjItOGZhYi1jZWYzOTA0NUIhkufemQifQ',
-//   },
-//  };
-// }
-
 export const login = creds => dispatch => {
  dispatch({ type: LOGIN_START });
+ // if we return axios call, we return a promise
+ return (
+  axiosWithAuth()
+   //  axios
+   //   .post('http://localhost:5000/api/login', creds)
+   .post('/login', creds)
+   .then(
+    res => {
+     localStorage.setItem('userToken', res.data.payload);
+     dispatch({
+      type: LOGIN_SUCCESS,
+     });
+     return true;
+    }
+    //    res => console.log(res.data.payload)
+   )
+   .catch(err => {
+    console.log('err: ', err.response.data.error);
+    dispatch({ type: ERROR, payload: err.response.data.error });
+   })
+ );
+};
+
+export const getFriends = () => dispatch => {
+ dispatch({ type: FETCH_START });
+ //  axios
+ axiosWithAuth()
+  .get('/friends')
+  //   .get('http://localhost:5000/api/friends')
+  .then(res => {
+   console.log('check data: ', res.data);
+   dispatch({ type: FETCH_SUCCESS, payload: res.data });
+  })
+
+  .catch(err =>
+   dispatch({ type: FETCH_ERROR, payload: err.response.data.error })
+  );
 };
